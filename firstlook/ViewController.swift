82716f410e7f991ae3193a34a8972077545953a0
Ijai Adnane <ijaiadnane@Ijais-iMac-3.local>
2016-09-23 10:04:38 -0700

    Boolean Fun

--- firstlook/ViewController.swift
+++ firstlook/ViewController.swift
@@ -9,30 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var myLabel: UILabel!
     @IBOutlet weak var text1: UITextField!
     
     @IBOutlet weak var text2: UITextField!
-    
+   
     
     @IBAction func buttonClick(_ sender: AnyObject) {
-        myLabel.text = "Answer is :\(Double(text1.text!)! + Double(text2.text!)!)"
-        
-        
-        
-        
+         let addition = false
+        if addition {
+            myLabel.text = "Answer is: \(Double(text1.text!)! + Double(text2.text!)!)"
+            
+            
+            
+            
+        } else {
+            myLabel.text = "Answer is: \(Double(text1.text!)! - Double(text2.text!)!)"
+            
+        }
     }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            // Do any additional setup after loading the view, typically from a nib.
+        }
+    
+        override func didReceiveMemoryWarning() {
+            super.didReceiveMemoryWarning()
+            // Dispose of any resources that can be recreated.
+        }
+        
+        
 }
 
